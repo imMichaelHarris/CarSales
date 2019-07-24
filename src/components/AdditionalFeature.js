40264419a6f3b2fed79049a0fb434c047a231ae3
Michael Harris <mhguitar1997@gmail.com>
2019-07-24 13:48:12 -0700

    Import add feature and connect in Additional feature component

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { addFeature } from "../actions";
 
 const AdditionalFeature = props => {
   return (
