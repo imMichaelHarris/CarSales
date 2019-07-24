17aef4387a34c7fd2de116606649739a17bdc109
Michael Harris <mhguitar1997@gmail.com>
2019-07-24 13:07:07 -0700

    Import Car Provider in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { CarProvider } from "./contexts/CarContext";
+
 import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
 
-const App = ({state}) => {
+const App = ({ state }) => {
   const removeFeature = item => {
     // dispatch an action here to remove an item
   };
