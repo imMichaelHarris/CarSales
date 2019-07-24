4c968d8dece009d033d87846e633e3019bfc5625
Michael Harris <mhguitar1997@gmail.com>
2019-07-24 12:45:28 -0700

    Wrap App component in react-redux provider

--- src/index.js
+++ src/index.js
@@ -9,4 +9,9 @@ import "bulma/css/bulma.css";
 import "./styles.scss";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  rootElement
+);
