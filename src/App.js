cb69b1462d514f1062094aefd86faafa4bb39aa2
Michael Harris <mhguitar1997@gmail.com>
2019-07-24 12:54:44 -0700

    Connect App to redux

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
 
 const App = () => {
-
-
   const removeFeature = item => {
     // dispatch an action here to remove an item
   };
@@ -30,4 +28,7 @@ const App = () => {
   );
 };
 
-export default App;
+export default connect(
+  null,
+  {}
+)(App);
